4172676dc9213bf252fa84e3a6050e2639e42241
William <4wprince@gmail.com>
2025-12-14 08:48:50 -0500

    Export APP_PASSWORD from config

--- src/config.js
+++ src/config.js
@@ -1,2 +1,5 @@
 export const API_URL =
   import.meta.env.VITE_API_URL || 'https://cfcorderbackend-sandbox.onrender.com'
+
+export const APP_PASSWORD =
+  import.meta.env.VITE_APP_PASSWORD || 'cfc2025'
